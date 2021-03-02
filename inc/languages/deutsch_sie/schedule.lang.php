e9cc9aeca6416a135b473dee7adef43108a1d32e
Julia Roloff <julia.roloff@qbeyond.de>
2021-03-02 18:18:25 +0100

    forum settings

--- inc/languages/deutsch_sie/schedule.lang.php
+++ inc/languages/deutsch_sie/schedule.lang.php
@@ -1,9 +1,13 @@
 <?php
 $l['schedule'] = "Post planen";
+$l['schedule_name'] = "Post/Threads planen";
+$l['schedule_forums'] = "Foren auswählen";
+$l['schedule_forums_desc'] = "In welchen Foren soll die Aktion aktiv sein?";
 $l['schedule_desc'] = "Soll dieser Post erst zu einem späteren Zeitpunkt veröffentlicht werden?";
 $l['schedule_posted'] = "Ihr Post wurde wie geplant veröffentlicht.";
 $l['schedule_error_no_draft'] = "Dieser Post muss als Entwurf gespeichert werden, da ein gewünschter Veröffentlichungszeitpunkt gewählt wurde.";
 $l['schedule_error_no_time'] = "Die Zeitangabe für die Planung des Posts ist nicht korrekt/vollständig.";
 $l['schedule_error_wrong_timing'] = "Der Post darf nicht für die Vergangenheit geplant werden!";
+$l['schedule_error_wrong_minutes'] = "Die geplante Zeit muss einer vollen Stunde entsprechen (z.B. 15:00, nicht 15:05)";
 $l['myalerts_setting_schedule_posted'] = "Benachrichtigung, wenn ein geplanter Post veröffentlicht wurde?";
 ?>
--- inc/plugins/schedule.php
+++ inc/plugins/schedule.php
@@ -6,18 +6,18 @@ if(!defined("IN_MYBB"))
 	die("Direct initialization of this file is not allowed.");
 }
 
-$plugins->add_hook("admin_formcontainer_output_row", "schedule_permission"); #done
-$plugins->add_hook("admin_user_groups_edit_commit", "schedule_permission_commit"); #done
-$plugins->add_hook("newthread_start", "schedule_newthread"); #TODO: Template + Form w date settings
-$plugins->add_hook("datahandler_post_validate_thread", "schedule_validate"); #done
+$plugins->add_hook("admin_formcontainer_output_row", "schedule_permission"); 
+$plugins->add_hook("admin_user_groups_edit_commit", "schedule_permission_commit"); 
+$plugins->add_hook("newthread_start", "schedule_newthread");
+$plugins->add_hook("datahandler_post_validate_thread", "schedule_validate");
 $plugins->add_hook("datahandler_post_validate_post", "schedule_validate");
-$plugins->add_hook("newthread_do_newthread_end", "schedule_do_newthread"); #TODO: get date settings into scheduled table
-$plugins->add_hook("editpost_end", "schedule_editpost");
+$plugins->add_hook("newthread_do_newthread_end", "schedule_do_newthread");
+$plugins->add_hook("editpost_start", "schedule_editpost");
 $plugins->add_hook("editpost_do_editpost_end", "schedule_do_editpost"); #TODO: edit date settings into scheduled table
-$plugins->add_hook("newreply_start", "schedule_newreply"); #TODO: Template + Form w date settings
-$plugins->add_hook("newreply_do_newreply_end", "schedule_do_newreply"); #TODO: get date settings into scheduled table
+$plugins->add_hook("newreply_start", "schedule_newreply"); 
+$plugins->add_hook("newreply_do_newreply_end", "schedule_do_newreply");
 if(class_exists('MybbStuff_MyAlerts_AlertTypeManager')) {
-	$plugins->add_hook("global_start", "schedule_alerts"); #done
+	$plugins->add_hook("global_start", "schedule_alerts"); #TODO: format alert
 }
 
 function schedule_info()
@@ -31,7 +31,7 @@ function schedule_info()
 		"website"		=> "https://github.com/ItsSparksFly",
 		"author"		=> "sparks fly",
 		"authorsite"	=> "https://github.com/ItsSparksFly",
-		"version"		=> "3.0",
+		"version"		=> "1.0",
 		"compatibility" => "18*"
 	);
 }
@@ -104,6 +104,36 @@ function schedule_install()
     while($theme = $db->fetch_array($tids)) {
         update_theme_stylesheet_list($theme['tid']);
     }
+
+	// settings
+	$setting_group = [
+		'name' => 'schedule',
+		'title' => $lang->schedule_name,
+		'description' => $lang->schedule_settings,
+		'disporder' => 5,
+		'isdefault' => 0
+	];
+
+	$gid = $db->insert_query("settinggroups", $setting_group);
+	
+	$setting_array = [
+		'ipt_inplay' => [
+			'title' => $lang->schedule_forums,
+			'description' => $lang->schedule_forums_desc,
+			'optionscode' => 'forumselect',
+			'disporder' => 1
+        ]
+    ];
+
+	foreach($setting_array as $name => $setting)
+	{
+		$setting['name'] = $name;
+		$setting['gid'] = $gid;
+
+		$db->insert_query('settings', $setting);
+	}
+
+	rebuild_settings();
 	
 }
 
@@ -141,6 +171,11 @@ function schedule_uninstall()
     while($theme = $db->fetch_array($query)) {
         update_theme_stylesheet_list($theme['tid']);
     }
+
+	// settings
+	$db->delete_query('settings', "name IN ('schedule_forums')");
+	$db->delete_query('settinggroups', "name = 'schedule'");
+	rebuild_settings();
 
 }
 function schedule_activate()
@@ -188,7 +223,7 @@ function schedule_activate()
 			<select name="schedule" class="div-toggle">
 				<option>Nein</option>
 				<option value="1" data-show=".with" {$selected}>Ja</option>
-			</select> <input type="date" name="sdate" value="{$sdate}" class="with hide" /> <input type="time" name="stime" value="{$stime}" class="with hide" />
+			</select> <input type="date" name="sdate" value="{$sdate}" class="with hide" /> <input type="time" name="stime" required step="3600" value="{$stime}" class="with hide" />
 		</td>
 	</tr>
 	</table>'),
@@ -249,7 +284,7 @@ function schedule_permission_commit()
 
 // load form template in newthread
 function schedule_newthread() {
-    global $mybb, $lang, $templates, $post_errors, $newthread_schedule;
+    global $mybb, $lang, $templates, $post_errors, $pid, $newthread_schedule;
     $lang->load('schedule');
     if($mybb->usergroup['canschedule'] == 1) {
         // previewing post?
@@ -259,6 +294,14 @@ function schedule_newthread() {
 			}
             $sdate = $mybb->get_input('sdate');
             $stime = $mybb->get_input('stime');
+        }
+		if($mybb->input['action'] == "editdraft") {
+			$scheduled = get_schedule($pid);
+			if($scheduled) {
+				$selected = "selected";
+				$sdate = date("Y-m-d", $scheduled['date']);
+				$stime = date("H:i", $scheduled['date']);
+			}
         }
         eval("\$newthread_schedule = \"".$templates->get("newthread_schedule")."\";");
     }
@@ -270,7 +313,7 @@ function schedule_validate(&$dh) {
 	$lang->load('schedule');
 	if($mybb->get_input('schedule') == 1 && $mybb->usergroup['canschedule'] == 1) {
 		// post is not saved as draft
-		if($mybb->get_input('savedraft') != 1) {
+		if(!$mybb->get_input('savedraft')) {
 			$dh->set_error($lang->schedule_error_no_draft);
 		}
 
@@ -285,6 +328,11 @@ function schedule_validate(&$dh) {
 		if($now > $sdate) {
 			$dh->set_error($lang->schedule_error_wrong_timing);
 		}
+
+		// even hours
+		if(date("i", $sdate) != "00") {
+			$dh->set_error($lang->schedule_error_wrong_minutes);
+		}
 	}
 }
 
@@ -292,12 +340,12 @@ function schedule_validate(&$dh) {
 function schedule_do_newthread() {
 	global $db, $mybb, $tid;
 	$ownuid = $mybb->user['uid'];
-	
+	$thread = get_thread($tid);
 	if($mybb->get_input('schedule') == 1) {
 		$sdate = strtotime("{$mybb->get_input('sdate')} {$mybb->get_input('stime')}");
 		$new_array = [
 			"tid" => $tid,
-			"pid" => 0,
+			"pid" => $thread['firstpost'],
 			"date" => $sdate,
 			"display" => 0
 		];
@@ -307,7 +355,7 @@ function schedule_do_newthread() {
 
 // load form template in newreply
 function schedule_newreply() {
-    global $mybb, $lang, $templates, $post_errors, $newreply_schedule;
+    global $mybb, $db, $lang, $templates, $post_errors, $pid, $newreply_schedule;
     $lang->load('schedule');
     if($mybb->usergroup['canschedule'] == 1) {
         // previewing post?
@@ -318,12 +366,37 @@ function schedule_newreply() {
             $sdate = $mybb->get_input('sdate');
             $stime = $mybb->get_input('stime');
         }
+		if($mybb->input['action'] == "editdraft") {
+			$scheduled = get_schedule($pid);
+			if($scheduled) {
+				$selected = "selected";
+				$sdate = date("Y-m-d", $scheduled['date']);
+				$stime = date("H:i", $scheduled['date']);
+			}
+        }
         eval("\$newreply_schedule = \"".$templates->get("newthread_schedule")."\";");
     }
 }
 
+function schedule_do_newreply() {
+	global $db, $mybb, $lang, $thread, $pid;
+	$ownuid = $mybb->user['uid'];
+
+	if($mybb->get_input('schedule') == 1) {
+		$sdate = strtotime("{$mybb->get_input('sdate')} {$mybb->get_input('stime')}");
+		$new_array = [
+			"tid" => $thread['tid'],
+			"pid" => $pid,
+			"date" => $sdate,
+			"display" => 0
+		];
+		$db->insert_query("scheduled", $new_array);
+	}
+
+}
+
 function schedule_editpost() {
-	global $mybb, $lang, $templates, $post_errors, $pid, $editpost_schedule;
+	global $mybb, $db, $lang, $templates, $post_errors, $pid, $editpost_schedule;
 	$lang->load('schedule');
 	if($mybb->usergroup['canschedule'] == 1) {
         if(isset($mybb->input['previewpost']) || $post_errors) {
@@ -334,8 +407,7 @@ function schedule_editpost() {
             $stime = $mybb->get_input('stime');
         }
 		else {
-			$query = $db->simple_select("scheduled", "*", "pid = '{$pid}'");
-			$scheduled = $db->fetch_array($query);
+			$scheduled = get_schedule($pid);
 			if($scheduled) {
 				$selected = "selected";
 				$sdate = date("Y-m-d", $scheduled['date']);
@@ -344,6 +416,10 @@ function schedule_editpost() {
 		}		
 		eval("\$editpost_schedule = \"".$templates->get("newthread_schedule")."\";");
 	}
+}
+
+function schedule_do_editpost() {
+
 }
 
 function schedule_alerts() {
@@ -409,4 +485,13 @@ function schedule_alerts() {
 	}
 
 }
+
+function get_schedule(int $pid) {
+	global $db;
+
+	$query = $db->simple_select("scheduled", "*", "pid = '{$pid}'");
+	$result = $db->fetch_array($query);
+	return $result;
+}
+
 ?>
