2606c2f94a1b3713f81acaf39d946c35c0e1b8a5
Julia Roloff <julia.roloff@qbeyond.de>
2021-03-02 20:42:37 +0100

    settings

--- inc/languages/deutsch_sie/schedule.lang.php
+++ inc/languages/deutsch_sie/schedule.lang.php
@@ -1,8 +1,5 @@
 <?php
 $l['schedule'] = "Post planen";
-$l['schedule_name'] = "Post/Threads planen";
-$l['schedule_forums'] = "Foren auswählen";
-$l['schedule_forums_desc'] = "In welchen Foren soll die Aktion aktiv sein?";
 $l['schedule_desc'] = "Soll dieser Post erst zu einem späteren Zeitpunkt veröffentlicht werden?";
 $l['schedule_posted'] = "Ihr Post wurde wie geplant veröffentlicht.";
 $l['schedule_error_no_draft'] = "Dieser Post muss als Entwurf gespeichert werden, da ein gewünschter Veröffentlichungszeitpunkt gewählt wurde.";
