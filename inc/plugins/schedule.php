af16c510c10c017243c41cf5cb89348b285f2af2
Julia Roloff <72280520+ItsSparksFly@users.noreply.github.com>
2021-04-28 11:30:57 +0200

    update threads via index

--- inc/plugins/schedule.php
+++ inc/plugins/schedule.php
@@ -15,6 +15,7 @@ $plugins->add_hook("newthread_do_newthread_end", "schedule_do_newthread");
 $plugins->add_hook("newreply_start", "schedule_newreply"); 
 $plugins->add_hook("newreply_do_newreply_end", "schedule_do_newreply");
 $plugins->add_hook("usercp_drafts_start", "schedule_drafts");
+$plugins->add_hook("index_end", "schedule_index");
 if(class_exists('MybbStuff_MyAlerts_AlertTypeManager')) {
 	$plugins->add_hook("global_start", "schedule_alerts");
 }
@@ -426,6 +427,72 @@ function schedule_drafts() {
 		}
 	}
 
+}
+
+function schedule_index() {
+	global $mybb, $db, $lang, $posthandler, $scheduled, $thread, $post, $new_thread, $valid_thread, $thread_info;
+   	$lang->load('schedule');
+	require_once MYBB_ROOT."inc/datahandlers/post.php";
+	$posthandler = new PostDataHandler("insert");
+	$now = TIME_NOW;
+	
+	$query = $db->simple_select("scheduled", "*", "date < '$now' AND display = 0");
+	while($scheduled = $db->fetch_array($query)) {
+
+		$thread = get_thread($scheduled['tid']);
+		$post = get_post($scheduled['pid']);
+
+		// we're updating a thread
+		if($thread['visible'] == "-2") {
+		    $posthandler->action = "thread";
+
+		    // Set the thread data that came from the input to the $thread array.
+		    $new_thread = array(
+			"fid" => $thread['fid'],
+			"subject" => $db->escape_string($thread['subject']),
+			"prefix" => "0",
+			"icon" => "0",
+			"uid" => $thread['uid'],
+			"username" => $thread['username'],
+			"message" => $db->escape_string($post['message']),
+			"ipaddress" => $session->packedip,
+			"dateline" => $scheduled['date']
+		    );
+
+		    $posthandler->set_data($new_thread);
+
+		    $valid_thread = $posthandler->validate_thread();
+
+		    $post_errors = array();
+		    // Fetch friendly error messages if this is an invalid thread
+		    if(!$valid_thread)
+		    {
+			$post_errors = $posthandler->get_friendly_errors();
+		    }
+
+		    foreach($post_errors as $error) {
+			echo $error;
+		    }
+
+		    $thread_info = $posthandler->insert_thread();
+		    $tid = $thread_info['tid'];
+
+		    $db->delete_query("threads", "tid = '{$scheduled['tid']}'");
+		    $db->delete_query("posts", "pid = '{$scheduled['pid']}'");
+
+		    if(class_exists('MybbStuff_MyAlerts_AlertTypeManager')) {
+			$alertType = MybbStuff_MyAlerts_AlertTypeManager::getInstance()->getByCode('schedule_posted');
+			if ($alertType != NULL && $alertType->getEnabled()) {
+			    $alert = new MybbStuff_MyAlerts_Entity_Alert((int)$mybb->user['uid'], $alertType, (int)$tid);
+			    MybbStuff_MyAlerts_AlertManager::getInstance()->addAlert($alert);
+			}
+		    }
+		}
+		$new_array = [
+		    "display" => 1
+		];
+		$db->update_query("scheduled", $new_array, "sid = '{$scheduled['sid']}'");
+	}
 }
 
 function schedule_alerts() {
